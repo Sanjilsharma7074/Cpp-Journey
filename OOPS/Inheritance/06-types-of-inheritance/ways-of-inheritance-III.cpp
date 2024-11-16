a0b65f666cfa3503b9d551cc834df50236167e6e
Sanjil Sharma <146429358+Sanjilsharma7074@users.noreply.github.com>
2024-11-16 16:36:01 +0530

    Add files via upload

---
+++ OOPS/Inheritance/06-types-of-inheritance/ways-of-inheritance-III.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class parent{
+
+    private:
+    int a;
+    
+    protected:
+    int b;
+
+    public:
+    int c;
+    void funP(){
+        a=10;
+        b=20;
+        c=30;
+    }
+};
+
+class child: protected parent{
+    void funC(){
+        // a=20;    will not access private 
+        b=30;
+        c=40;
+    }
+};
+
+class grandChild: public child{
+    void funG(){
+        // a=30;    will not access private members of parent class but can access the protected members.
+        b=40;
+        c=50;
+    }
+};
+
+
+int main(){
+
+    child c;
+    // c.a=5;   will not access private and protected members of child class.
+    // c.b=3;
+    // c.c=8;
+    return 0;
+}
